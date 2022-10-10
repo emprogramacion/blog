6509c6841dc7b0d30ea1d51141b5dbe223d42b1a
Erasmo Martinez <em.programacion14@gmail.com>
2022-10-10 00:10:17 -0500

    Mostrar imagen al editar y validar si hay o no una imagen con una alerta

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -19,39 +19,27 @@
                             <label>Título *</label>
                             <input type="text" name="title" class="form-control" required value="{{ old('title', $post->title) }}">
                         </div>
-                        <div class="form-group">
+                        <!-- <div class="form-group">
                             <label>Image</label>
                             <input type="file" name="file" class="form-control-file">
-                        </div>
-
-                    <!-- Inicio: Código para validar imagen en el post y mostrarla, si no muestra un mensaje diciendo que no tiene  -->
-                        <!-- @if (!empty($post->image))
-                          <div class="col-12">
-                            <label>Imagen</label>
-                          </div>
-                          <div class="center">
-                            <img 
-                              src="{{ url('storage/'. old('image', $post->image)) }}" 
-                              style="width: 60%; height: 80%"  
-                              class="rounded mx-auto d-block"
-                              alt="image"
-                            >
-                          </div> 
-                        @else
-                          <div class="alert alert-secondary" role="alert">
-                            Este post no contiene imagen de portada!
-                          </div>  
-                        @endif -->
-                    <!-- Fin: Código para validar imagen en el post y mostrarla, si no muestra un mensaje diciendo que no tiene  -->
-
-                    
-                    <!-- Inicio: Código para traer la imagen al formulario de editar -->
-                        <!-- <div class="form-group row">
-                            <label class="col-8">Imagen</label>
-                                <img class="col-6 offset-3" src="{{url('storage/'.$post->image)}}" alt="">
-                                <input class="col-12 mt-4" type="file" name="file">
                         </div> -->
-                    <!-- Fin: Código para traer la imagen al formulario de editar -->
+
+                        <!-- Inicio: Código para traer la imagen al formulario de editar y validando imagen -->
+                        <div class="form-group">
+                            <label>Imagen</label>
+                            @if(!empty($post->image))
+                            <div class="div">
+                                <img class="col-3 offset-4" src="{{ url('storage/'. old('image', $post->image)) }}" alt="image">
+                                <input class="col-12 mt-3 offset-3" type="file" name="file">
+                            </div>
+                            @else
+                            <div class="alert alert-secondary" role="alert">
+                                ¡Este post no contiene imagen de portada!
+                            </div>
+                            <input class="form-control-file" type="file" name="file">
+                            @endif
+                        </div>
+                        <!-- Fin: Código para traer la imagen al formulario de editar  y validando imagen-->
 
                         <div class="form-group">
                             <label>Contenido *</label>
